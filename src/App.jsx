437a21a644520f7650360c08c1ff045c19754d78
Aatif Sohel <84847205+aatifsohel@users.noreply.github.com>
2025-09-15 20:02:43 +0530

    build form split component

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,10 @@ function App() {
     <div className="app">
       <div className="sidebar">
         <FriendsList />
+        <FormAddFriend />
+        <Button>Add Friend</Button>
       </div>
+      <FormSplitBill />
     </div>
   );
 }
@@ -68,6 +71,43 @@ function Friend({ friend }) {
 
 function Button({ children }) {
   return <button className="button">{children}</button>;
+}
+
+function FormAddFriend() {
+  return (
+    <form className="form-add-friend">
+      <label>🧑Friend name</label>
+      <input type="text" />
+
+      <label>🖼️Image URL</label>
+      <input type="text" />
+
+      <Button>Add</Button>
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with X</h2>
+
+      <label>💰Bill value</label>
+      <input type="text" />
+
+      <label>🫰Your expense</label>
+      <input type="text" />
+
+      <label>🧑X's expense</label>
+      <input type="text" disabled />
+
+      <label>🤑 Who is paying the bill</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
+    </form>
+  );
 }
 
 export default App;
